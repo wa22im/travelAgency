82511ee3e3b2d7d6e099d9753320c27b8568d3d1
wassim <wassim22m@gmail.com>
2019-11-17 00:23:16 -1000

    Implementing CRUD services for voyage fixing  bugs and checking

--- src/main/java/com/ditraacademy/travelagency/core/destination/DestinationService.java
+++ src/main/java/com/ditraacademy/travelagency/core/destination/DestinationService.java
@@ -1,12 +1,9 @@
 package com.ditraacademy.travelagency.core.destination;
 
-import com.ditraacademy.travelagency.core.voyage.Voyage;
-import com.ditraacademy.travelagency.utils.AcceptedResponseModel;
 import com.ditraacademy.travelagency.utils.ErrorResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import sun.security.krb5.internal.crypto.Des;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,19 +16,23 @@ public class DestinationService {
 
     public ResponseEntity<?> updateDestination(int id , Destination destination) {
         Optional<Destination> destinationOptional = destinationRepository.findById(id);
-        if ( !(destinationOptional.isPresent())){
-            return  new ResponseEntity<>(new ErrorResponseModel("Id not found "), HttpStatus.BAD_REQUEST); }
-        Destination destinationlegacy = destinationOptional.get();
-        if ( destination.getNom()!=null || destination.getNom()!=""){
-            destinationlegacy.setNom(destination.getNom());
-        }
-        if ( destination.getDescription()!=null || destination.getDescription()!=""){
-            destinationlegacy.setDescription(destination.getDescription());
-        }
-        destinationRepository.deleteById(id);
-        destinationRepository.save(destinationlegacy);
 
-        return  new ResponseEntity<>(new AcceptedResponseModel("Updated "), HttpStatus.OK) ;
+        if (!destinationOptional.isPresent())
+            return  new ResponseEntity<>(new ErrorResponseModel("Id not found "), HttpStatus.BAD_REQUEST);
+
+        Destination destinationLegacy = destinationOptional.get();
+
+        if ((destination.getNom() != null) || !(destination.getNom().equals(destinationLegacy.getNom()))){
+            destinationLegacy.setNom(destination.getNom());
+        }
+
+        if (destination.getDescription() != null){
+            destinationLegacy.setDescription(destination.getDescription());
+        }
+
+        destinationRepository.save(destinationLegacy);
+
+        return  new ResponseEntity<>( HttpStatus.OK) ;
     }
 
 
@@ -50,11 +51,9 @@ public class DestinationService {
 
 
     public ResponseEntity<?> addDestination(Destination destination) {
-        if (destination.getNom().length() < 2) {
-            return new ResponseEntity<>(new ErrorResponseModel("Error nom destination non valide "),
-                    HttpStatus.BAD_REQUEST); }
-        destinationRepository.save(destination);
-        return  new ResponseEntity<>(HttpStatus.OK); }
+
+        destination= destinationRepository.save(destination);
+        return  new ResponseEntity<>(destination,HttpStatus.OK); }
 
 
     public ResponseEntity<?> deleteDestination(int id) {
@@ -64,7 +63,7 @@ public class DestinationService {
             new ResponseEntity<>(new ErrorResponseModel("id not found"), HttpStatus.BAD_REQUEST);
         }
         destinationRepository.deleteById(id);
-        return   new ResponseEntity<>(new AcceptedResponseModel("Deleted"),HttpStatus.OK)  ;
+        return   new ResponseEntity<>(HttpStatus.OK)  ;
 
     }
 
--- src/main/java/com/ditraacademy/travelagency/core/voyage/VoyageServices.java
+++ src/main/java/com/ditraacademy/travelagency/core/voyage/VoyageServices.java
@@ -1,8 +1,14 @@
 package com.ditraacademy.travelagency.core.voyage;
 
+import com.ditraacademy.travelagency.core.destination.Destination;
+import com.ditraacademy.travelagency.utils.ErrorResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VoyageServices {
@@ -11,18 +17,66 @@ public class VoyageServices {
     VoyageRepository voyageRepository ;
     // put
     public ResponseEntity<?> updateVoyage(int id , Voyage voyage ) {
-        return  null ; }
+        Optional<Voyage> optionalVoyage =  voyageRepository.findById(id);
+
+        if (!optionalVoyage.isPresent())
+            return new ResponseEntity<>(new ErrorResponseModel(" Invalid voyage"), HttpStatus.BAD_REQUEST);
+
+        Voyage voyageLegacy = optionalVoyage.get();
+
+        if ( voyage.getDate()!=null ||   voyageLegacy.getDate() != voyage.getDate())
+            voyageLegacy.setDate(voyage.getDate());
+
+        if (   voyage.getDescription()!=null || !(voyageLegacy.getDescription().equals(voyage.getDescription())))
+            voyageLegacy.setDescription(voyage.getDescription());
+
+        if (  voyage.getNbPlaces()!=voyageLegacy.getNbPlaces())
+            voyageLegacy.setNbPlaces(voyage.getNbPlaces());
+
+        if ( voyage.getPrix()!=voyageLegacy.getPrix())
+            voyageLegacy.setPrix(voyage.getPrix());
+
+        if ( voyage.getTitre()!= null || !(voyage.getTitre().equals(voyageLegacy.getTitre())))
+            voyageLegacy.setTitre(voyage.getTitre());
+
+        voyageRepository.save(voyageLegacy);
+
+        return  new ResponseEntity<>(HttpStatus.OK) ; }
+
+
     // Get a voyage
     public ResponseEntity<?> getVoyage(int id) {
-        return  null ; }
+        Optional<Voyage> optionalVoyage  =  voyageRepository.findById(id);
+        if(!(optionalVoyage.isPresent())){
+            return  new ResponseEntity<>(new ErrorResponseModel("ID not found "), HttpStatus.BAD_REQUEST); }
+        return  new ResponseEntity<>(optionalVoyage.get(),HttpStatus.OK) ;
+         }
+
+
     // Get all voyages
     public ResponseEntity<?> getAllVoyages() {
-        return  null ; }
+        List<Voyage> voyages = voyageRepository.findAll();
+        return  new  ResponseEntity<>(voyages ,HttpStatus.OK) ;
+         }
+
+
     // post  a voyage
     public ResponseEntity<?> addVoyage( Voyage voyage) {
-        return  null ; }
+        voyage  = voyageRepository.save(voyage);
+        return  new ResponseEntity<>(voyage,HttpStatus.OK);
+       }
+
+
+
     // Delete voyage
     public ResponseEntity<?> deleteVoyage(int id) {
-        return  null ; }
+        Optional< Voyage > optionalVoyage =  voyageRepository.findById(id);
+
+        if (!(optionalVoyage.isPresent())) {
+            new ResponseEntity<>(new ErrorResponseModel("id not found"), HttpStatus.BAD_REQUEST);
+        }
+        voyageRepository.deleteById(id);
+        return   new ResponseEntity<>(HttpStatus.OK)  ;
+    }
 
 }
